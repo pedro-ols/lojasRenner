3614a29859a7b71d19fb1c93e00458316ece2ed3
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-05-29 09:18:13 -0300

    feat: create profile tab

---
+++ app/profile.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text style={{fontSize: '30px', color: '#6A5ACD',}}>Bem-vindo</Text>
+            <Text style={{fontSize: '30px', color: '#6A5ACD',}}>ao</Text>
+            <Text style={{fontSize: '30px', color: '#6A5ACD',}}>Aplicativo</Text>
+        </View>
+    )
+}
