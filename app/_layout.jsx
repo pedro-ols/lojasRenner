f0dda95d5437b5908a5c7c62953393f562791591
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-05-29 09:15:56 -0300

    feat: create pages and change tab links

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,57 +1,70 @@
-import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
-
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native-web";
 
 export default function Layout() {
-    return (
-        <Tabs screenOptions={{
-            tabBarActiveTintColor: "#007AFF",
-            tabBarInactiveTintColor: "#8E8E93",
-            tabBarStyle: {
-                backgroundColor: "#FFF",
-                borderTopWidth: 1,
-                borderTopColor: "#E0E0E0",
-                height: 60,
-            },
-            tabBarLabelStyle: {
-                fontSize: 12,
-                marginBottom: 5,
-            },
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#007AFF",
+        tabBarInactiveTintColor: "#8E8E93",
+        tabBarStyle: {
+          backgroundColor: "#FFF",
+          borderTopWidth: 1,
+          borderTopColor: "#E0E0E0",
+          height: 60,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          marginBottom: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
         }}
-        >
-            <Tabs.Screen name="index"
-                options={{
-                    title: "Home",
-                    tabBarIcon: ({ color, size }) =>
-                    (<Ionicons name="home" color={color} size={size} />
-                    ),
-                }}
-            />
-            <Tabs.Screen name="profile"
-                options={{
-                    title: "Perfil",
-                    tabBarIcon: ({ color, size }) =>
-                    (<Ionicons name="person" color={color} size={size} />
-                    ),
-                }}
-            />
-
-
-            <Tabs.Screen name="new"
-                options={{
-                    title: "Noticias",
-                    tabBarIcon: ({ color, size }) =>
-                    (<Ionicons name="newspaper-outline" color={color} size={size} />
-                    ),
-                }}
-            />
-
-
-
-
-
-
-        </Tabs>
-
-    )
+      />
+      <Tabs.Screen
+        name="blog"
+        options={{
+          title: "Blog",
+          tabBarIcon: ({ color, size }) => (
+            <View style={{ width: 15, height: 15, borderRadius: 100, borderWidth: 1 }}/>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="menu"
+        options={{
+          title: "Menu",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="menu" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cards"
+        options={{
+          title: "Cartões",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="card" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
